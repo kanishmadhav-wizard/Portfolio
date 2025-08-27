512d5bebacba8323288a3c2107519618ffeede78
kanishmadhav <kanishmadhavguru@gmail.com>
2025-08-27 15:58:15 +0530

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -58,7 +58,7 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=Kanishmadhav-wizard&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=kanishmadhav-wizard&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="Kanishmadhav"
             loading="lazy"
           />
@@ -84,7 +84,7 @@ const AboutDetails = () => {
 
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <Link
-            href="https://github.com/kanishmadhav-wizard/Nextjs-contentlayer-blog"
+            href="https://github.com/kanishmadhav-wizard/LWYMPA"
             target="_blank"
             className="w-full"
           >
